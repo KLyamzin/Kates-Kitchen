1974258baeb23aaedb0a929d9433bc51d1dbf9ad
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-07-18 12:32:17 -0400

    added Menu assets

--- src/menu.js
+++ src/menu.js
@@ -1,2 +1,10 @@
-const renderMenu = () => {};
+import { resetMain } from "./reset";
+import coffeeMachine from "./assets/pexels-michael-burrows-7125662.jpg";
+import sandwich from "./assets/pexels-suzy-hazelwood-1209029.jpg";
+import eggDish from "./assets/pexels-kiro-wang-7463313.png";
+import desert from "./assets/pexels-engin-akyurt-2478319.jpg";
+const renderMenu = () => {
+  const mainDiv = document.querySelector(".main");
+  resetMain(mainDiv);
+};
 export { renderMenu };
