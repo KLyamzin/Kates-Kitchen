c925e2f8b861a2f66bee0ec043fe0f1deff9542b
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-07-19 19:32:27 -0400

    finished the About tab

--- src/about.js
+++ src/about.js
@@ -1,6 +1,32 @@
 import { resetMain } from "./reset";
 import staffImage from "./assets/pexels-ketut-subiyanto-4349918.jpeg";
+import personOneImage from "./assets/pexels-ketut-subiyanto-4353583.jpg";
+import personTwoImage from "./assets/pexels-ketut-subiyanto-4349815.jpg";
+import personThreeImage from "./assets/pexels-ketut-subiyanto-4349736.jpg";
 
+const persons = [
+  {
+    name: "Annabel",
+    image: personOneImage,
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicingelit. Vel, porro.",
+  },
+  {
+    name: "Jackie",
+    image: personTwoImage,
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicingelit. Vel, porro.",
+  },
+  {
+    name: "Laura",
+    image: personThreeImage,
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicingelit. Vel, porro.",
+  },
+];
+
+const messageTextOne =
+  "Not only are we, Kate's Kitchen, passionate about our local community and coffee, but we are also just as passionate about our employees. Our baristas and bakers are what create our foundation of memorable flavor and seamless service.";
 const renderAbout = () => {
   const mainDiv = document.querySelector(".main");
   resetMain(mainDiv);
@@ -8,13 +34,9 @@ const renderAbout = () => {
   const placeNameContainer = document.createElement("div");
   placeNameContainer.classList.add("place-name-container");
 
-  const placeName = document.createElement("p");
-  placeName.classList.add("place-name");
-  placeName.innerText = "About Us";
-
   const placeNameSub = document.createElement("p");
-  placeNameSub.classList.add("place-name-sub");
-  placeNameSub.innerText = "Meet Our Amazing Staff";
+  placeNameSub.classList.add("food-header");
+  placeNameSub.innerText = "MEET THE AMAZING STAFF";
 
   const placeOwner = document.createElement("img");
   placeOwner.classList.add("place-owner");
@@ -22,13 +44,33 @@ const renderAbout = () => {
 
   const placeWelcome = document.createElement("p");
   placeWelcome.classList.add("place-welcome");
-  placeWelcome.innerText = `Our stuff:`;
+  placeWelcome.classList.add("welcomeOne");
+  placeWelcome.innerText = messageTextOne;
 
-  placeNameContainer.appendChild(placeName);
+  const allPersonsContainer = document.createElement("div");
+  allPersonsContainer.className = "all-persons-container";
   placeNameContainer.appendChild(placeNameSub);
   mainDiv.appendChild(placeNameContainer);
   mainDiv.appendChild(placeOwner);
   mainDiv.appendChild(placeWelcome);
+  mainDiv.appendChild(allPersonsContainer);
+
+  persons.forEach((person) => {
+    const personContainer = document.createElement("div");
+    personContainer.className = "person-container";
+    const personImage = document.createElement("img");
+    const personName = document.createElement("h1");
+    const personQuote = document.createElement("p");
+    const name = person.name;
+    const quote = person.quote;
+    personImage.setAttribute("src", person.image);
+    personName.innerText = name;
+    personQuote.innerText = quote;
+    personContainer.appendChild(personImage);
+    personContainer.appendChild(personName);
+    personContainer.appendChild(personQuote);
+    allPersonsContainer.appendChild(personContainer);
+  });
 };
 
 export { renderAbout };
