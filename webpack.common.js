9746902d26e4ebc5fb0ecb3737c342c7c2d3a21d
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-07-20 21:47:21 -0400

    Created Assets folder in dist

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     clean: true,
-    assetModuleFilename: "[name][ext]",
+    assetModuleFilename: "assets/[name][ext]",
   },
   plugins: [
     new HtmlWebpackPlugin({
